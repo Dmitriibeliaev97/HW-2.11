fa22c65f5af64ec06ccebb98e0045c3d50bcd6c9
Dmitrii Beliaev <dmitriymay97@gmail.com>
2023-06-03 10:47:40 +0300

    Внес изменения

--- src/main/java/Spring/Skypro/HW21/controllers/ShoppingCartController.java
+++ src/main/java/Spring/Skypro/HW21/controllers/ShoppingCartController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Set;
 
 @RestController
-@RequestMapping("/store/order")
+@RequestMapping("/order")
 public class ShoppingCartController {
     private final ShoppingCartService shoppingCartService;
 
